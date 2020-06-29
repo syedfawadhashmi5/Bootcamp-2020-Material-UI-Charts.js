129320bbd8ba07a291ed7f9fdfcfaa3473b52071
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-06-29 02:00:26 -0700

    update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ButtonAppBar from './Appbar'
+import SimpleContainer from './card'
+import './App.css';
+
+// CREATE A SIMPLE FUNTION TO ORDER APP
+
+
+
+function App() {
+  return (
+    <div>
+<ButtonAppBar />
+<SimpleContainer />
+    </div>
+  )
+}
+
+// CREATE A SIMPLE FUNTION TO ORDER APP
+
+
+export default App;
